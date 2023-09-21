6f5622cb463c92c4f5bf608fd20745c7a5c66b0b
Rishabh7889 <124988817+Rishabh7889@users.noreply.github.com>
2023-09-21 17:46:41 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
    try {
       console.log(process.env.MONGO_URI)
-      const conn = await mongoose.connect(process.env.MONGO_URI);
+      const conn = await mongoose.connect("mongodb+srv://gargrishabh7889:Rishabh7889@cluster0.ongg5im.mongodb.net/");
 
       console.log(` MongoDB Connection Established\n`);
    } catch (error) {
